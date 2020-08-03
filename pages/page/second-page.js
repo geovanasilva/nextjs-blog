63c4e4b4620d1fe3bf72565c49da14ccdc2947b0
Geovana Silva <sts.geovana@gmail.com>
2020-08-02 21:38:43 -0300

    feat: create a new page to deal with routing

---
+++ pages/page/second-page.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Empty page</title>
+      </Head>
+      <h1>I'm sorry, this page is empty 😕</h1>
+    </Layout>
+  );
+}
